2b375c5b70bc442de19c66260327604a7fded4c7
Thomas Hawley <thawley22@turing.edu>
2023-03-07 09:17:34 -0700

    test: Add tests for #check_stock and #stock

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 RSpec.describe Vendor do
   before(:each) do
     @vendor = Vendor.new("Rocky Mountain Fresh")
+
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
   end
 
   describe '#initialize' do
@@ -16,6 +19,20 @@ RSpec.describe Vendor do
 
     it 'starts with an empty hash for inventory' do
       expect(@vendor.inventory).to eq({})
+    end
+  end
+
+  describe '#check_stock' do
+    it 'can return 0 if that item is not stocked' do
+      expect(@vendor.check_stock(@item1)).to eq(0)
+    end
+  end
+
+  describe '#stock' do
+    it 'can add an item and amount to inventory' do
+      @vendor.stock(@item1, 30)
+
+      expect(@vendory.inventory).to eq({@item1 => 30})
     end
   end
 end
