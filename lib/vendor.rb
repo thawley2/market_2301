cd9ccc8dd4f1a611244ce4f8fb63353b89512fd4
Thomas Hawley <thawley22@turing.edu>
2023-03-07 09:20:20 -0700

    feat: Add #stock method

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -5,4 +5,12 @@ class Vendor
     @name = name
     @inventory = Hash.new(0)
   end
+
+  def check_stock(item)
+    @inventory[item]
+  end
+
+  def stock(item, amount)
+    @inventory[item] += amount
+  end
 end
