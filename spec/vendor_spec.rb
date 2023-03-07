8bc54ddfe42c4c2fd27187bc2c7ef617443cc681
Thomas Hawley <thawley22@turing.edu>
2023-03-07 09:23:05 -0700

    test: Make tests more robust

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -25,14 +25,26 @@ RSpec.describe Vendor do
   describe '#check_stock' do
     it 'can return 0 if that item is not stocked' do
       expect(@vendor.check_stock(@item1)).to eq(0)
+
+      @vendor.stock(@item1, 30)
+
+      expect(@vendor.check_stock(@item1)).to eq(30)
     end
   end
 
   describe '#stock' do
     it 'can add an item and amount to inventory' do
       @vendor.stock(@item1, 30)
+      @vendor.stock(@item2, 12)
 
-      expect(@vendory.inventory).to eq({@item1 => 30})
+      expect(@vendor.inventory).to eq({@item1 => 30, @item2 => 12})
+    end
+
+    it 'can add additional stock if that item is already stocked' do
+      @vendor.stock(@item1, 30)
+      @vendor.stock(@item1, 25)
+
+      expect(@vendor.check_stock(@item1)).to eq(55)
     end
   end
 end
