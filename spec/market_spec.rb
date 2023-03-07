478743513c2df4c56f6f031f1b06044fb9dee699
Thomas Hawley <thawley22@turing.edu>
2023-03-07 10:05:48 -0700

    test: Add #sorted_item_list test

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe Market do
   before(:each) do
     @market = Market.new("South Pearl Street Farmers Market")
 
-    @vendor1 = Vendor.new("Rocky Mountain Fresh")
-    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
-    @vendor3 = Vendor.new("Palisade Peach Shack")
+    @vendor1 = Vendor.new('Rocky Mountain Fresh')
+    @vendor2 = Vendor.new('Ba-Nom-a-Nom')
+    @vendor3 = Vendor.new('Palisade Peach Shack')
 
-    @item1 = Item.new({name: 'Peach', price: "$0.75"})
-    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
-    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-    @item5 = Item.new({name: "Tacos", price: "$0.99"})
+    @item1 = Item.new({name: 'Peach', price: '$0.75'})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @item3 = Item.new({name: 'Peach-Raspberry Nice Cream', price: '$5.30'})
+    @item4 = Item.new({name: 'Banana Nice Cream', price: '$4.25'})
+    @item5 = Item.new({name: 'Tacos', price: '$0.99'})
 
     @vendor1.stock(@item1, 35) 
     @vendor1.stock(@item2, 7)
@@ -64,6 +64,16 @@ RSpec.describe Market do
       expect(@market.vendors_that_sell(@item1)).to eq([@vendor1, @vendor3])
       expect(@market.vendors_that_sell(@item4)).to eq([@vendor2])
       expect(@market.vendors_that_sell(@item5)).to eq([])
+    end
+  end
+
+  describe '#sorted_item_list' do
+    it 'can return a unique list of all items sorted alphabetically' do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+
+      expect(@market.sorted_item_list).to eq(['Banana Nice Cream', 'Peach', 'Peach-Raspberry Nice Cream', 'Tomato'])
     end
   end
 end
